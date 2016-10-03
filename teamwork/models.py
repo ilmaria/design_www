596ee8879d80442735fdcb218e057a85ba674e7b
Ilmari Autio <ilmari.autio@gmail.com>
2016-10-03 15:45:58 +0300

    Added comments

--- teamwork/models.py
+++ teamwork/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 class Project(models.Model):
+    """Has all information related to one project."""
+
     name = models.CharField(max_length=255)
 
     def __str__(self):
@@ -8,6 +10,10 @@ class Project(models.Model):
 
 
 class Event(models.Model):
+    """Describes different events that are related to one project.
+    TODO: Should deadlines be different model than events?
+    """
+
     date = models.DateTimeField()
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
 
--- teamwork/views.py
+++ teamwork/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 
 def project_details(request):
+    """Project detail page for viewing all details related to one project.
+    View all events, deadlines, time used for one project.
+    """
+    
     return render(request, 'project_details.html')
 
 
 def dashboard(request):
+    """Dashboard for viewing all current projects at the same time and
+    seeing details for each project, like next event or deadline. 
+    """
+
     return render(request, 'dashboard.html')

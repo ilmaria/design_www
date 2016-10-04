f687f335c54ae5144242ec81a8dc2b9f9ba0511d
Ilmari Autio <ilmari.autio@gmail.com>
2016-10-04 20:33:39 +0300

    Added new fields to Project model

--- teamwork/models.py
+++ teamwork/models.py
@@ -1,10 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class Student(models.Model):
+    """Application users."""
+
+    # `user` field references to User model that holds the login information
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
 
 class Project(models.Model):
     """Has all information related to one project."""
 
-    name = models.CharField(max_length=255)
-    course = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
+    course = models.CharField(max_length=255, blank=True)
+    members = models.ManyToManyField(Student, related_name='projects')
+    description = models.CharField(max_length=1000, blank=True)
 
     def __str__(self):
         return "\"{0}\"".format(self.name)
@@ -15,8 +25,10 @@ class Event(models.Model):
     TODO: Should deadlines be different model than events?
     """
 
+    name = models.CharField(max_length=255, default="New Event")
     date = models.DateTimeField()
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    location = models.CharField(max_length=255, blank=True)
 
     def __str__(self):
         return str(self.project) + ', ' + str(self.date)

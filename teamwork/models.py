918b3ffb6b810755bac2078b497f3e6ef88d0f8c
naolle <loan.le.3879@gmail.com>
2016-11-01 12:17:45 +0200

    add event type to model.py

--- teamwork/models.py
+++ teamwork/models.py
@@ -30,6 +30,7 @@ class Event(models.Model):
     date = models.DateTimeField()
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     location = models.CharField(max_length=255, blank=True)
+    type = models.CharField(max_length=255, default='event')
 
     def __str__(self):
         return 'Event: "{0}" - {1} - {2}'.format(self.name, self.project, self.date)

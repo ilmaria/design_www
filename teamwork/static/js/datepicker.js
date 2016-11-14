d1a35e83cb405c1f5b2da18030d31ee9bb7d367b
naolle <loan.le.3879@gmail.com>
2016-11-15 01:27:03 +0200

    remove validation

--- teamwork/static/js/datepicker.js
+++ teamwork/static/js/datepicker.js
@@ -4,27 +4,5 @@ $(document).ready(function() {
             autoclose: true,
             format: 'dd/mm/yyyy',
             todayBtn: 'linked'
-        })
-
-    $('#logtimeForm').formValidation({
-        framework: 'bootstrap',
-        icon: {
-            valid: 'glyphicon glyphicon-ok',
-            invalid: 'glyphicon glyphicon-remove',
-            validating: 'glyphicon glyphicon-refresh'
-        },
-        fields: {
-            date: {
-                validators: {
-                    notEmpty: {
-                        message: 'The date is required'
-                    },
-                    date: {
-                        format: 'DD/MM/YYYY',
-                        message: 'The date is not a valid'
-                    }
-                }
-            }
-        }
-    });
+        });
 });

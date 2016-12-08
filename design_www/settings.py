fa9ab86ea653fede032641994509f0e629036846
Ilmari Autio <ilmari.autio@gmail.com>
2016-12-08 17:49:44 +0200

    Update Heroku files

--- design_www/settings.py
+++ design_www/settings.py
@@ -13,19 +13,19 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'mwd$wmgyf7_7ya-qa*71%$e$)b3_9z*gqce@raa1$(qn9qsf#$'
+SECRET_KEY = os.getenv('SECRET_KEY', 'mwd$wmgyf7_7ya-qa*71%$e$)b3_9z*gqce@raa1$(qn9qsf#$')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', True)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.localhost', '.herokuapp.com']
 
 LOGIN_URL = '/login/'
 
@@ -121,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
